3a0edee9cff09b7145ee087c57fe6c9738cfccca
Javey <jiawei23716@sina.com>
2015-12-01 22:28:29 +0800

    emulate api

--- js/pages/chatDetail.js
+++ js/pages/chatDetail.js
@@ -1,32 +1,7 @@
-define(['intact', 'tpl/chatDetail', 'underscore'], function(Intact, template, _) {
+define(['intact', 'tpl/chatDetail', 'underscore', 'js/lib/utils'], function(Intact, template, _, utils) {
     return Intact.extend({
         defaults: {
-            chats: [{
-                id: 0,
-                name: 'Ben Sparrow',
-                lastText: 'You on your way?',
-                face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-            }, {
-                id: 1,
-                name: 'Max Lynx',
-                lastText: 'Hey, it\'s me',
-                face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-            }, {
-                id: 2,
-                name: 'Adam Bradleyson',
-                lastText: 'I should buy a boat',
-                face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-            }, {
-                id: 3,
-                name: 'Perry Governor',
-                lastText: 'Look at my mukluks!',
-                face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-            }, {
-                id: 4,
-                name: 'Mike Harrington',
-                lastText: 'This is wicked good ice cream.',
-                face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-            }]
+            chats: []
         },
 
         displayName: 'ChatDetail',
@@ -34,15 +9,17 @@ define(['intact', 'tpl/chatDetail', 'underscore'], function(Intact, template, _)
         template: template,
 
         _init: function() {
-            this._fetch();
+            return this._fetch();
         },
 
         _fetch: function() {
-            var chat = _.find(this.get('chats'), function(chat) {
-                return chat.id == this.get('id');
-            }, this);
+            return utils.api().then(function(data) {
+                var chat = _.find(data, function(chat) {
+                    return chat.id == this.get('id');
+                }, this);
 
-            this.set('chat', chat);
+                this.set('chat', chat);
+            }.bind(this));
         }
     });
 });
